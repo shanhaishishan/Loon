301753aedfecfbdb6ba4764f20460e1a1729746a
mist-whisper <114045385+mist-whisper@users.noreply.github.com>
2024-12-11 16:08:52 +0800

    Update Unfold.js

--- Script/Unfold/Unfold.js
+++ Script/Unfold/Unfold.js
@@ -11,8 +11,8 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/mist-whisper/Loon/refs/heads/master/Script/Unfold/Unfold.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/mist-whisper/Loon/refs/heads/master/Script/Unfold/Unfold.js
 
 [mitm]
 hostname = api.revenuecat.com
